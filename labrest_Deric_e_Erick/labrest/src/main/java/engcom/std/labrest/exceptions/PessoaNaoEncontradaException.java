20a8bd69d67fabfb6d256313af5fb0644def605d
dericcarvalho <128261935+dericcarvalho@users.noreply.github.com>
2023-03-18 20:25:24 -0300

    Add files via upload

---
+++ labrest_Deric_e_Erick/labrest/src/main/java/engcom/std/labrest/exceptions/PessoaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package engcom.std.labrest.exceptions;
+
+public class PessoaNaoEncontradaException extends RuntimeException {
+    public PessoaNaoEncontradaException(long id) {
+        super("Não foi possível encontrar pessoa com o id: " + id);
+    }
+}
